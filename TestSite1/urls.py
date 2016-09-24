662d4ef5b34a466c05fac7179d937128ba8ba570
Kaiwen Yu <yu356@purdue.edu>
2016-09-23 20:46:32 -0400

    before ignore

--- TestSite1/urls.py
+++ TestSite1/urls.py
@@ -5,4 +5,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^training/', include(admin.site.urls)),
 ]
